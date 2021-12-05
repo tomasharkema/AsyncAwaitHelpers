2bea49258d911e5ef664e1e2131393cd8a190a7c
Tomas Harkema <tomas@harkema.io>
2021-12-05 17:26:28 +0100

    fix a lot of things

--- Sources/AsyncAwaitHelpers/Once.swift
+++ Sources/AsyncAwaitHelpers/Once.swift
@@ -11,14 +11,39 @@ public struct NoKey: Hashable {}
 
 public typealias OnceSingle<Value, Error: Swift.Error> = Once<NoKey, Value, Error>
 
-public actor Once<Key: Hashable, Value, Error: Swift.Error> {
+private actor OnceData<Key: Hashable, Value, Error: Swift.Error>: Sendable {
+  var tasks = [Key: (Date, Task<Value, Error>)]()
+
+  func insert(key: Key, value: (Date, Task<Value, Error>)) {
+    tasks[key] = value
+  }
+
+  func delete(key: Key) {
+    tasks[key] = nil
+  }
+
+  func cancel(key: Key) {
+    tasks[key]?.1.cancel()
+    tasks[key] = nil
+  }
+
+  func cancelAll() {
+    for task in tasks.values {
+      task.1.cancel()
+    }
+
+    tasks = [:]
+  }
+}
+
+public class Once<Key: Hashable, Value, Error: Swift.Error> {
   public init() {}
 
-  private var task: [Key: (Date, Task<Value, Error>)] = [:]
+  private let data = OnceData<Key, Value, Error>()
 
   @discardableResult
-  public func onceKeepOriginal(key: Key, keepInCache: TimeInterval? = nil, _ handler: @autoclosure () -> Task<Value, Error>) -> Task<Value, Error> {
-    if let task = task[key] {
+  public func onceKeepOriginal(key: Key, keepInCache: TimeInterval? = nil, _ handler: @autoclosure () -> Task<Value, Error>) async -> Task<Value, Error> {
+    if let task = await data.tasks[key] {
       guard let keepInCache = keepInCache else {
         return task.1
       }
@@ -31,25 +56,25 @@ public actor Once<Key: Hashable, Value, Error: Swift.Error> {
     let newTask = handler()
     let date = Date()
 
-    Task {
-      _ = await newTask.result
-      if let keepInCache = keepInCache {
-        let sleep = max(0, keepInCache - abs(date.timeIntervalSinceNow))
-        print("SLEEP FOR \(sleep)")
-        try await Task.sleep(time: sleep)
-        print("SLEPT! INVALIDATING")
+    Task.detached(priority: .background) {
+      await self.data.insert(key: key, value: (date, newTask))
+
+      do {
+        let _ = try await newTask.value
+        if keepInCache == nil {
+          await self.data.delete(key: key)
+        }
+      } catch {
+        await self.data.delete(key: key)
       }
-      self.task[key] = nil
     }
 
-    task[key] = (date, newTask)
     return newTask
   }
 
-
   @discardableResult
-  public func onceKeepOriginal(keepInCache: TimeInterval? = nil, _ handler: @autoclosure () -> Task<Value, Error>) -> Task<Value, Error> where Key == NoKey {
-    return onceKeepOriginal(key: NoKey(), keepInCache: keepInCache, handler())
+  public func onceKeepOriginal(keepInCache: TimeInterval? = nil, _ handler: @autoclosure () -> Task<Value, Error>) async -> Task<Value, Error> where Key == NoKey {
+    return await onceKeepOriginal(key: NoKey(), keepInCache: keepInCache, handler())
   }
 
   @discardableResult
@@ -85,24 +110,24 @@ public actor Once<Key: Hashable, Value, Error: Swift.Error> {
   }
 
   @discardableResult
-  public func onceCancelOriginal(key: Key, _ handler: @autoclosure () -> Task<Value, Error>) -> Task<Value, Error> {
-    task[key]?.1.cancel()
-    task[key] = nil
+  public func onceCancelOriginal(key: Key, _ handler: @autoclosure () -> Task<Value, Error>) async -> Task<Value, Error> {
+    await data.cancel(key: key)
 
     let newTask = handler()
 
-    Task {
+    Task.detached(priority: .background) {
+      await self.data.insert(key: key, value: (Date(), newTask))
       _ = await newTask.result
-      self.task[key] = nil
+      await self.data.delete(key: key)
     }
 
-    task[key] = (Date(), newTask)
+
     return newTask
   }
 
   @discardableResult
-  public func onceCancelOriginal(_ handler: @autoclosure () -> Task<Value, Error>) -> Task<Value, Error> where Key == NoKey {
-    return onceCancelOriginal(key: NoKey(), handler())
+  public func onceCancelOriginal(_ handler: @autoclosure () -> Task<Value, Error>) async -> Task<Value, Error> where Key == NoKey {
+    return await onceCancelOriginal(key: NoKey(), handler())
   }
 
   @discardableResult
@@ -139,33 +164,63 @@ public actor Once<Key: Hashable, Value, Error: Swift.Error> {
   {
     await onceCancelOriginal(key: NoKey(), handler)
   }
+}
 
-  private func _cancel(key: Key) {
-    task[key]?.1.cancel()
-    task[key] = nil
+// MARK: Cancellation
+
+extension Once {
+
+  public func cancel(key: Key) async {
+    await data.cancel(key: key)
   }
 
-  private func _cancelAll() {
-    for task in task {
-      task.value.1.cancel()
-    }
-    task = [:]
-  }
-
-  public nonisolated func cancel(key: Key) {
+  public nonisolated func cancelAllAndForget() {
     Task {
-      await _cancel(key: key)
+      await cancelAll()
     }
   }
 
-  public nonisolated func cancel() where Key == NoKey {
-    cancel(key: NoKey())
+  public func cancelAll() async {
+    await data.cancelAll()
   }
 
-  public nonisolated func cancelAll() {
-    Task {
-      await _cancelAll()
-    }
+  public func cancel() async where Key == NoKey {
+    await cancel(key: NoKey())
   }
 }
 
+// MARK: onceOnlyIfNeeded
+
+extension Once {
+  public func onceOnlyIfNeeded(key: Key, _ handler: @autoclosure () -> Task<Value, Error>) async throws -> Value? {
+    if await data.tasks[key] != nil {
+      return nil
+    }
+
+    return try await onceKeepOriginal(key: key, handler()).value
+  }
+
+  public func onceOnlyIfNeeded(_ handler: @autoclosure () -> Task<Value, Error>) async -> Value? where Error == Never, Key == NoKey {
+    if await data.tasks[NoKey()] != nil {
+      return nil
+    }
+
+    return await onceKeepOriginal(handler()).value
+  }
+
+  public func onceOnlyIfNeeded(key: Key, _ handler: @escaping @Sendable () async -> Value) async throws
+  -> Value? where Error == Swift.Error
+  {
+    try await onceOnlyIfNeeded(key: key, Task {
+      await handler()
+    })
+  }
+
+  public func onceOnlyIfNeeded(_ handler: @escaping @Sendable () async -> Value) async
+  -> Value? where Error == Never, Key == NoKey
+  {
+    await onceOnlyIfNeeded(Task {
+      await handler()
+    })
+  }
+}
---
+++ Sources/AsyncAwaitHelpers/SingleAccessActor.swift
@@ -0,0 +1,17 @@
+//
+//  SingleAccessActor.swift
+//  
+//
+//  Created by Tomas Harkema on 27/11/2021.
+//
+
+import Foundation
+
+//@globalActor
+//public actor SingleAccessActor {
+//  public static let shared: SingleAccessActor = SingleAccessActor()
+//
+//  public static func run<T>(resultType: T.Type = T.self, body: @SingleAccessActor @Sendable () throws -> T) async rethrows -> T {
+//    return try await body()
+//  }
+//}
